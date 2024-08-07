e843190b888cf1b3bc9b050908f980759f6a8df0
Chris Brannen <cbrannen@outlook.com>
2024-08-07 09:23:16 +0100

    feat: adding db

--- index-ws.js
+++ index-ws.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const server = require("http").createServer();
-
 const app = express();
 
 app.get("/", function (req, res) {
@@ -8,23 +7,35 @@ app.get("/", function (req, res) {
 });
 
 server.on("request", app);
-server.listen(3000, function () {
-  console.log("Server started");
+
+process.on("SIGINT", () => {
+  server.close(() => {
+    shutdownDB();
+  });
 });
 
-/** Websocket */
-const WebsocketServer = require("ws").Server;
+server.listen(3000, function () {
+  console.log("Listening on 3000");
+});
 
-const wss = new WebsocketServer({ server });
+/** Websocket **/
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server: server });
 
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
-  console.log("Clients connected", numClients);
 
-  wss.broadcast(`Current visitors :${numClients}`);
+  console.log("clients connected: ", numClients);
+
+  // Log number of visitors at current moment
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))`);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
-    ws.send("Welcome to my server");
+    ws.send("welcome!");
   }
 
   ws.on("close", function close() {
@@ -32,13 +43,45 @@ wss.on("connection", function connection(ws) {
     console.log("A client has disconnected");
   });
 
-  ws.on("error", function error() {
-    //
-  });
+  ws.on("error", function error() {});
 });
 
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
 wss.broadcast = function broadcast(data) {
+  console.log("Broadcasting: ", data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
+/** End Websocket **/
+
+/** Database stuff **/
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(":memory:");
+
+// .seralize ensures DB is set up before any queries
+db.serialize(() => {
+  db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down DB");
+  db.close();
+}
+
+/** End Database stuff **/
